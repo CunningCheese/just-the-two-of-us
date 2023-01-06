d354ddca27281b8a937bbee1a9eac36b0c4068dc
SonicBoom2o <keenuvermeulen@gmail.com>
2023-01-06 16:04:18 +0200

    added custom api_key

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 #code to automate the acquisition of "just the two of us" images from youtube and crop them
+
 import numpy as np
 import os
 import cv2
 # import youtube_dl
 from glob import glob
 import pafy
+
+from api_key import key # requires a api_key.py file containing the variable 'key' with a google api key as the value
+
+frame_num = 180 #nth frame to catch 180 = 3s @60fps
+pafy.set_api_key(key)
 
 def create_dir(path):
     try:
@@ -14,29 +20,25 @@ def create_dir(path):
         print(f"ERROR: creating director with name {path}")
 
 def save_frame(video_stream, save_dir): #get frame from video
+
     create_dir(save_dir)
 
     cap = cv2.VideoCapture(video_stream.url)
-    frame_num = 260 #nth frame to catch
-
-    cap.set(1, frame_num) #(1, x) x = which frame to capture
-
+    cap.set(1, frame_num) #(1, x) x = whichth frame to capture
     ret, frame = cap.read()
 
     cv2.imwrite(f"{save_dir}\\{frame_num}.png", frame)
 
     cap.release()
+    print("Completed Capturing Frame " + frame_num)
 
 
-if __name__ == "__main__":
-    url = "https://www.youtube.com/watch?v=X-pZFNsncu8" #youtube url
+if __name__ == "__main__": #you know the drill
+    url = "https://www.youtube.com/watch?v=X-pZFNsncu8"
 
-    vid_stream = pafy.new(url)
-    video = vid_stream.getbest(preftype="mp4")
+    print("Starting...")
+    vid_stream = pafy.new(url) 
+    video = vid_stream.getbestvideo(preftype="mp4")
+
+    print("Running...")
     save_frame(video, "saves")
-
-    # video_paths = glob("videos/*")
-    # save_dir = "save"
-
-    # for path in video_paths:
-    #     save_frame(path, save_dir)
